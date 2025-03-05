3a08cc9f8a2d2ed9257348c2089e4e2eb4600ee5
Jason Franklin <jason.franklin@xing.com>
2025-03-05 15:11:37 +0100

    Add Agendas

---
+++ app/controllers/agenda_controller.rb
@@ -0,0 +1,4 @@
+class AgendaController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "agenda/index"
   get "storytelling/index"
   resources :registrations, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

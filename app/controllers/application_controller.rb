51efa25012a954f13e22f68aecb23b8894241575
Jason Franklin <jason.franklin@xing.com>
2025-03-11 12:55:39 +0100

    Allow non-modern browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
